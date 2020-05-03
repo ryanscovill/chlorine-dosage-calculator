575d48947d93c826b701afde546d245198f588f4
Ryan Scovill <rscovill@sfu.ca>
2020-05-03 00:14:29 -0700

    add basic input handling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,85 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Container, TextField } from "@material-ui/core";
+import { Container, TextField, InputAdornment } from "@material-ui/core";
 import InputSelect from "./components/InputSelect";
 
 class App extends Component {
+  state = { volume: 0, dosage: 0, percent: 0, chlorine: 0 };
 
-  state = {volume: 0, dosage: 0, percent: 0, ppm: 0, chlorine: 0}
-
-  updateState = (variable) => (event) => {
-    this.setState({ [variable]: event.target.value});
+  updateState = (variable) => (value) => {
+    this.setState({ [variable]: value });
     this.calculateChlorine();
-  }
+  };
 
   calculateChlorine = () => {
-    let amount = (this.state.ppm * this.state.volume) / (this.state.percent * 10000)
-    this.setState({chlorine: amount})
-  }
+    let amount =
+      (this.state.dosage * this.state.volume) / (this.state.percent * 10000);
+    this.setState({ chlorine: amount });
+  };
 
   render() {
     return (
       <div className="App">
         <Container maxWidth="md">
           <form noValidate autoComplete="off">
-            <InputSelect label="Volume" onChange={this.updateState("volume")} />
-            <TextField label="Dosage" onChange={this.updateState("dosage")} />
-            <TextField label="Percent" onChange={this.updateState("percent")} />
-            <TextField label="PPM" onChange={this.updateState("ppm")} />
+          <div class="text-input">
+            <InputSelect
+              className="text-input"
+              label="Volume"
+              onChange={this.updateState("volume")}
+              units={[
+                { name: "cubic meters", factor: 1 },
+                { name: "liters", factor: 1000 },
+                { name: "US Gallon", factor: 264.172 },
+                { name: "Gallon", factor: 219.969204701183 },
+              ]}
+            />
+            </div>
+            <div className="text-input">
+            <TextField
+              label="Dosage"
+              onChange={(e) => this.updateState("dosage")(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">mg/L</InputAdornment>
+                ),
+              }}
+            />
+            </div>
+            <div className="text-input">
+            <TextField
+              className="text-input"
+              label="Percent"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">%</InputAdornment>
+                ),
+              }}
+              onChange={(e) => this.updateState("percent")(e.target.value)}
+            />
+            </div>
           </form>
-          <h1>Chlorine: {this.state.chlorine}</h1>
+          <div className="text-input">
+          <h3 style={{marginTop: "3em"}}>Chlorine Required</h3>
+            <InputSelect
+              className="text-input"
+              value={this.state.chlorine}
+              InputProps={{
+                readOnly: true
+              }}
+              units={[
+                { name: "cubic meters", factor: 1 },
+                { name: "liters", factor: 1000 },
+                { name: "US Gallon", factor: 264.172 },
+                { name: "Gallon", factor: 219.969204701183 },
+              ]}
+            />
+            </div>
         </Container>
       </div>
     );
-    }
+  }
 }
 
 export default App;
--- src/components/InputSelect.jsx
+++ src/components/InputSelect.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
-import { TextField } from '@material-ui/core'
+import React from "react";
+import { TextField, Select, MenuItem } from "@material-ui/core";
 
 class InputSelect extends React.Component {
-    constructor() {
-        super();
-        let state = {value: null}
+  constructor(props) {
+    super(props);
+    this.state = { };
+    let { units } = this.props;
+    this.state = {factor: units[0].factor, value: '', standardizedValue: ''};
+  }
 
-    }
+  handleSelectChange = (e) => {
+      let factor = e.target.value
+      this.setState({factor: factor});
+      this.setState({value: this.state.standardizedValue * factor})
+  }
 
+  handleInputChange = (e) => {
+      this.setState({value: e.target.value, standardizedValue: e.target.value / this.state.factor}, () => this.props.onChange(this.state.standardizedValue));
+  }
 
-    render() {
-        return (
-            <div><TextField {...this.props} /></div>
-        );
-    }
-
+  render() {
+    let { units, onChange, ...otherProps } = this.props;
+    return (
+      <div style={{ display: "flex" }}>
+        <TextField {...otherProps} value={this.state.value} onChange={this.handleInputChange} />
+        <Select displayEmpty value={this.state.factor} onChange={this.handleSelectChange}>
+          {units.map((unit) => (
+            <MenuItem key={unit.name} value={unit.factor}>{unit.name}</MenuItem>
+          ))}
+        </Select>
+      </div>
+    );
+  }
 }
-
 
 export default InputSelect;
