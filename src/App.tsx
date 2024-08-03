59bdf233c413e32a5a96cd7b36d685823a5d9e57
ryanscovill <rscovill@sfu.ca>
2024-08-03 11:41:12 -0700

    fix: update chlorine state dosage
    - make container full width

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import "./App.css";
-import { Container, InputAdornment, Typography, Button, ThemeProvider, CssBaseline } from "@mui/material";
+import { Container, InputAdornment, Typography, Button, ThemeProvider, CssBaseline, Grid } from "@mui/material";
 import InputSelect from "./components/InputSelect";
 import NumberField from "./components/NumberField";
 import React from "react";
@@ -14,27 +13,23 @@ const App: React.FC = () => {
   const units = [
     { name: "Liters", factor: 1000 },
     { name: "Cubic Meters", factor: 1 },
-    { name: "Imperial Gallons", factor: 219.969204701183 },
-    { name: "US Gallons", factor: 264.172 },
+    { name: "Gallons (Imperial)", factor: 219.969204701183 },
+    { name: "Gallons (US)", factor: 264.172 },
   ];
 
   const updateState = (variable: string) => (value: any) => {
     if (isNaN(Number(value))) {
       return;
     }
-    setState((prevState) => ({ ...prevState, [variable]: value, isFormReset: false }));
-    calculateChlorine()
+    setState((prevState) => {
+      const newState = { ...prevState, [variable]: value, isFormReset: false };
+      let chlorineAmount = calculateChlorine(newState);
+      return { ...newState, chlorine: chlorineAmount };
+    });
   };
 
-  const calculateChlorine = () => {
-    let amount = parseFloat(
-      (((parseFloat(state.dosage) * (parseFloat(state.volume) / state.volumeFactor)) / (parseFloat(state.percent) * 10000)).toFixed(4)).toString()
-    );
-    let chlorineRequired = 0;
-    if (!isNaN(amount) && isFinite(amount)) {
-      chlorineRequired = amount;
-    }
-    setState((prevState) => ({ ...prevState, chlorine: chlorineRequired }));
+  const calculateChlorine = (s: typeof initialState) => {
+    return parseFloat(((parseFloat(s.dosage) * (parseFloat(s.volume) / s.volumeFactor)) / (parseFloat(s.percent) * 10000)).toFixed(4));    
   };
 
   const clearState = () => {
@@ -47,7 +42,7 @@ const App: React.FC = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="App">
-        <Container maxWidth="md">
+        <Container maxWidth="sm">
           <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <Typography variant="h4" style={{ marginBottom: '.5em', marginTop: '.5em' }}>Chlorine Dosage Calculator</Typography>
             {!state.isFormReset && (
@@ -55,40 +50,45 @@ const App: React.FC = () => {
             )}
           </div>
           <form noValidate autoComplete="off">
-            <div className="text-input">
-              <InputSelect
-                label="Volume"
-                onInputChange={updateState("volume")}
-                onSelectChange={updateState("volumeFactor")}
-                factor={state.volumeFactor}
-                value={state.volume}
-                units={units}
-              />
-            </div>
-            <div className="text-input">
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <InputSelect
+                  label="Volume"
+                  onInputChange={updateState("volume")}
+                  onSelectChange={updateState("volumeFactor")}
+                  factor={state.volumeFactor}
+                  value={state.volume}
+                  units={units}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
               <NumberField
                 label="Dosage"
                 onChange={(e: { target: { value: any; }; }) => updateState("dosage")(e.target.value)}
                 value={state.dosage}
                 InputProps={{
                   endAdornment: (
-                    <InputAdornment position="end">mg/L</InputAdornment>
+                    <InputAdornment position="end">mg/L or ppm</InputAdornment>
                   ),
                 }}
+                fullWidth
               />
-            </div>
-            <div className="text-input">
-              <NumberField
-                label="Percent"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">%</InputAdornment>
-                  ),
-                }}
-                value={state.percent}
-                onChange={(e: { target: { value: any; }; }) => updateState("percent")(e.target.value)}
-              />
-            </div>
+              </Grid>
+              <Grid item xs={12}>
+                <NumberField
+                  label="Chlorine Concentration %"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">%</InputAdornment>
+                    ),
+                  }}
+                  value={state.percent}
+                  onChange={(e: { target: { value: any; }; }) => updateState("percent")(e.target.value)}
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
           </form>
           <div className="text-input">
             <Typography variant="h5" style={{ marginTop: "2em" }}>Chlorine Required</Typography>
@@ -100,6 +100,8 @@ const App: React.FC = () => {
               }}
               onSelectChange={updateState("chlorineFactor")}
               units={units}
+              error={isNaN(state.chlorine) || !isFinite(state.chlorine)}
+              fullWidth
             />
           </div>
         </Container>
